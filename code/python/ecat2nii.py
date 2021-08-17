ba12e0f981f32173352eb28f388f4483b130bada
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2021-08-17 18:03:09 -0400

    Add sif file python (#19)
    * added sif output to ecat2nii.py, need to revert back to
    
    previous matlab ecat2nii to verify output is the same. Will merge into main once done.
    
    * fixed scaling in ecat2nii.py

--- code/python/ecat2nii.py
+++ code/python/ecat2nii.py
@@ -1,3 +1,5 @@
+import datetime
+
 import nibabel
 import numpy
 import pathlib
@@ -22,6 +24,7 @@ def ecat2nii(ecat_main_header=None,
         nifti_file = nifti_file
     # collect the output folder from the nifti path will use for .sif files
     output_folder = pathlib.Path(nifti_file).parent
+    nifti_file_w_out_extension = os.path.splitext(str(pathlib.Path(nifti_file).name))[0]
 
     # if already read nifti file skip re-reading
     if ecat_main_header is None and ecat_subheaders is None and ecat_pixel_data is None and ecat_file:
@@ -79,13 +82,13 @@ def ecat2nii(ecat_main_header=None,
         # save out our slice of data before flip to a text file to compare w/ matlab data
         img_temp[:, :, :, index] = numpy.flip(numpy.flip(numpy.flip(
             data[:, :, :, index].astype(numpy.dtype('>f4')) * sub_headers[index]['SCALE_FACTOR'], 1), 2), 0)
-        start.append(sub_headers[index]['FRAME_START_TIME'] * 60)  # scale to per minute
-        delta.append(sub_headers[index]['FRAME_DURATION'] * 60)  # scale to per minute
+        start.append(sub_headers[index]['FRAME_START_TIME'] * 0.001)  # scale to per minute
+        delta.append(sub_headers[index]['FRAME_DURATION'] * 0.001)  # scale to per minute
 
         if main_header.get('SW_VERSION', 0) >= 73:
             # scale both to per minute
-            prompts.append(sub_headers[index]['PROMPT_RATE'] * sub_headers[index]['FRAME_DURATION'] * 60)
-            randoms.append(sub_headers[index]['RANDOM_RATE'] * sub_headers[index]['FRAME_DURATION'] * 60)
+            prompts.append(sub_headers[index]['PROMPT_RATE'] * sub_headers[index]['FRAME_DURATION'] * 0.001)
+            randoms.append(sub_headers[index]['RANDOM_RATE'] * sub_headers[index]['FRAME_DURATION'] * 0.001)
         else:
             # this field is not available in ecat 7.2
             prompts.append(0)
@@ -202,6 +205,16 @@ def ecat2nii(ecat_main_header=None,
 
     # write out timing file
     if sif_out:
-        pass
+        with open(os.path.join(output_folder, nifti_file_w_out_extension + '.sif'), 'w') as siffile:
+            scantime = datetime.datetime.fromtimestamp(main_header['SCAN_START_TIME'])
+            scantime = scantime.astimezone().isoformat()
+            siffile.write(f"{scantime} {len(start)} 4 1\n")
+            for index in reversed(range(len(start))):
+                start_i = round(start[index])
+                start_i_plus_delta_i = start_i + round(delta[index])
+                prompt = round(prompts[index])
+                random = round(randoms[index])
+                output_string = f"{start_i} {start_i_plus_delta_i} {prompt} {random}\n"
+                siffile.write(output_string)
 
     return img_nii
