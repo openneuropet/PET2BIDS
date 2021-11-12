07094eeb231a7e635908b15cf6c128acd063f4b6
Cyril Pernet <wamcyril@gmail.com>
2021-11-12 12:16:57 +0100

    updating existent json fields

--- matlab/updatejsonpetfile.m
+++ matlab/updatejsonpetfile.m
@@ -1,11 +1,34 @@
-function status = updatejsonpetfile(filename,newfields)
+function status = updatejsonpetfile(jsonfilename,newfields,dcminfo)
+
+% generic function that updates PET json file with missing PET-BIDS
+% information
+%
+% FORMAT status = updatejsonpetfile(jsonfilename,newfields,dcminfo)
+%
+% INPUT jsonfilename jspon file to update
+%       newfields is a structure with the newfields to go into the json file
+%       dcminfo a dcmfile or the dicominfo structure frmo a representative
+%       file -- this info is used to also update the json file, and if a comflict
+%       exists it returns warning messages, assumnimg the newfield provided is 
+%       correct (i.e. as a user you know better than default dicom)
+%
+% OUTPUT status returns the state of the updating (includes warning messages
+%               returned if any)
+%
+% Cyril Pernet Nov 2021
+% ----------------------------------------------
+% Copyright Open NeuroPET team
+
+status = struct('state',[],'messages',{});
+status_index = 1;
 
 %% check data in
 % current file metadata
-if exist(jsontoload,'file')
-    filemetadata = jsondecode(fileread(filename));
+if exist(jsonfilename,'file')
+    filemetadata = jsondecode(fileread(jsonfilename));
+    filemetadatafields = fieldnames(filemetadata);
 else
-    error('looking for %s, but the file is missing',jsontoload)
+    error('looking for %s, but the file is missing',jsonfilename)
 end
 
 % proposed metadata
@@ -22,6 +45,23 @@ else
     error('looking for %s, but the file is missing',jsontoload)
 end
 
+%% possible dcm fields to recover
+% - this part is truly empirical, going over different dcm files and
+% figuring out fields
+
+if exist('dcminfo','var')
+    if ischar(dcminfo)
+        dcminfo = flattenstruct(dicominfo(dcminfo));
+    else
+        dcminfo = flattenstruct(dcminfo);
+    end
+else
+    error('%s does not exist',dcminfo)
+end
+
+
+
+
 %% run the update
 
 % some dcm2nixx fields can be updated (depends on dcm2nixx version)
@@ -30,7 +70,48 @@ if isfield(filemetadata,'Unit')
     filemetadata       = rmfield(filemetadata,'Unit');
 end
 
+if isfield(filemetadata,'ReconstructionMethod')
+    filemetadata.ReconMethodName = filemetadata.ReconstructionMethod;
+    filemetadata                 = rmfield(filemetadata,'ReconstructionMethod');
+end
 
+if isfield(filemetadata,'ConvolutionKernel')
+    if contains(filemetadata.ConvolutionKernel,'.00')
+        loc = findstr(filemetadata.ConvolutionKernel,'.00');
+        filemetadata.ConvolutionKernel(loc:loc+2) = [];
+        filtersize = regexp(filemetadata.ConvolutionKernel,'\d*','Match');
+        if ~isempty(filtersize)
+            filemetadata.ReconFilterSize = cell2mat(filtersize);
+            loc = findstr(filemetadata.ConvolutionKernel,filtersize{1});
+            filemetadata.ConvolutionKernel(loc:loc+length(filemetadata.ReconFilterSize)-1) = [];
+            filemetadata.ReconFilterType = filemetadata.ConvolutionKernel;
+        else
+            filemetadata.ReconFilterType = filemetadata.ConvolutionKernel;
+        end
+        filemetadata = rmfield(filemetadata,'ConvolutionKernel');
+    end
+end
+
+missing_index = 1;
+for f=1:length(petmetadata.mandatory)
+    test = contains(filemetadatafields,petmetadata.mandatory{f},'IgnoreCase',true);
+    if any(test)
+        exactmatch = find(strcmpi(filemetadatafields(find(test)),petmetadata.mandatory{f}));
+        if exactmatch == 0
+            missing{missing_index} = petmetadata.mandatory{f};
+            missing_index = missing_index + 1;
+        end
+    else
+       missing{missing_index} = petmetadata.mandatory{f};
+       missing_index = missing_index + 1;
+    end
+end
+
+if ~exist('missing','var')
+    warning('All mandatory fields were found in the json file before any updates')
+    status.messages{status_index} = 'All mandatory fields were found in the json file before any updates';
+    status_index = status_index+1;
+end
 
 % check all fields
 for m=length(petmetadata.mandatory):-1:1
@@ -43,6 +124,10 @@ else
     
 end
 
-
+% finish up
+if isfield(filemetadata,'ConversionSoftware')
+    filemetadata.ConversionSoftware = [filemetadata.ConversionSoftware ' - json edited with ONP updatejsonpetfile.m'];
+end
+jsonwrite(jsonfilename,filemetadata)
 
 
