eef925d207291366b1a9fae53422835f3fe236bf
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2021-11-15 08:53:27 -0500

    Ecat writer (#36)
    * drafting write header
    
    * better
    
    * simplified keeping track of order
    
    * refactoring and adding struct datatypes to json
    
    * new get header method works, need to test on subheaders
    
    * write header test passed
    
    * fixed test
    
    * testing write on real ecat
    
    * removed quotes from string
    
    * caching downloads
    
    * hard coded
    
    * full path
    
    * write directory works
    
    * fixed off by 1 index error
    
    * byte blocks for ecat are 512....fixed reading & writing to suit.
    
    * ranges are off, but this is mostly there
    
    * Cleaned up write ecat, read ecat, and golden ecat
    
    Most of the way there, initial table creation was causing a cascade
    of errors. Now fixed. Still not writing 100% accurately, getting errors
    after main header and directory table are successfully written.
    
    * partial commit, about to fix one off error in directory
    
    * added 2 byte positioning to dir table
    
    * updated ecat read to use struct formatting"
    
    * passes writing tests

--- pypet2bids/tests/test_ecatread.py
+++ pypet2bids/tests/test_ecatread.py
@@ -4,6 +4,21 @@ from scipy.io import savemat
 import dotenv
 import os
 import nibabel
+import unittest
+import json
+import pathlib
+
+
+parent_dir = pathlib.Path(__file__).parent.resolve()
+code_dir = parent_dir.parent
+data_dir = code_dir.parent
+
+# collect ecat header maps
+try:
+    with open(os.path.join(code_dir, 'ecat_headers.json'), 'r') as infile:
+        ecat_header_maps = json.load(infile)
+except FileNotFoundError:
+    raise Exception("Unable to load header definitions and map from ecat_headers.json. Aborting.")
 
 """
 This script reads in an ecat file from .env/environment variable 'TEST_ECAT_PATH' and saves the pixel data into a 
@@ -13,35 +28,35 @@ repeat this operation using nibabel's ecat.load and storing the output at 'NIBAB
 The error handling for the nibabel write to matlab demonstrate that saving the larger nibabel generated array can fail
 with a sufficiently large ecat file.
 """
-
-
 # collect path to test ecat file
 dotenv.load_dotenv(dotenv.find_dotenv())  # load path from .env file into env
 ecat_path = os.environ['TEST_ECAT_PATH']
 read_ecat_save_as_matlab = os.environ['READ_ECAT_SAVE_AS_MATLAB']
 nibabel_read_ecat_save_as_matlab = os.environ['NIBABEL_READ_ECAT_SAVE_AS_MATLAB']
 
-# read in the ecat
-ecat_main_header, ecat_subheaders, ecat_image = read_ecat(ecat_file=ecat_path)
 
-# read in the ecat with nibabel
-nibabel_ecat = nibabel.ecat.load(ecat_path)
-# extract the image data from nibabel
-nibabel_data = nibabel_ecat.get_fdata()
+if __name__ == '__main__':
+    # read in the ecat
+    ecat_main_header, ecat_subheaders, ecat_image = read_ecat(ecat_file=ecat_path)
 
-# save the read ecat pixel object as a matlab object for comparison
-matlab_dictionary = {'data': ecat_image}
-savemat(read_ecat_save_as_matlab, matlab_dictionary)
-print(f"Saved read ecat datastructure as matlab matrix at:\n{read_ecat_save_as_matlab}")
+    # read in the ecat with nibabel
+    nibabel_ecat = nibabel.ecat.load(ecat_path)
+    # extract the image data from nibabel
+    nibabel_data = nibabel_ecat.get_fdata()
 
-# save the nibabel ecat read as matlab object for comparison
-matlab_dictionary = {'nibabel_data': nibabel_data}
-try:
-    savemat(nibabel_read_ecat_save_as_matlab, matlab_dictionary, do_compression=True)
-    print(f"Saved read ecat datastructure as matlab matrix at:\n{nibabel_read_ecat_save_as_matlab}")
-except scipy.io.matlab.miobase.MatWriteError as err:
-    print(f"Unable to write nibabel array of size {nibabel_data.shape} and "
-          f"datatype {nibabel_data.dtype} to matlab .mat file")
-    print(err)
+    # save the read ecat pixel object as a matlab object for comparison
+    matlab_dictionary = {'data': ecat_image}
+    savemat(read_ecat_save_as_matlab, matlab_dictionary)
+    print(f"Saved read ecat datastructure as matlab matrix at:\n{read_ecat_save_as_matlab}")
+
+    # save the nibabel ecat read as matlab object for comparison
+    matlab_dictionary = {'nibabel_data': nibabel_data}
+    try:
+        savemat(nibabel_read_ecat_save_as_matlab, matlab_dictionary, do_compression=True)
+        print(f"Saved read ecat datastructure as matlab matrix at:\n{nibabel_read_ecat_save_as_matlab}")
+    except scipy.io.matlab.miobase.MatWriteError as err:
+        print(f"Unable to write nibabel array of size {nibabel_data.shape} and "
+              f"datatype {nibabel_data.dtype} to matlab .mat file")
+        print(err)
 
 
