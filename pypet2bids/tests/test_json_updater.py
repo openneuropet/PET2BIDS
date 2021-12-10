a5bf3cc6091ffa72eba1169c90317c8002732231
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2021-12-10 16:36:32 -0500

    Add example spreadsheet reader (#50)
    * update entry point in toml
    
    * added example of spreadsheet reader to spreadsheet folder

---
+++ pypet2bids/tests/test_json_updater.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+
+
+class TestJsonUpdater(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_load(self):
+        with open('test.json', 'w'):
+            pass
+
+
+if __name__ == '__main__':
+    unittest.main()
