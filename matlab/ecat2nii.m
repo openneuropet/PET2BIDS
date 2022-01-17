72786d76ddd6ce794cee89d85fb3d7f94e5247d5
CPernet <wamcyril@gmail.com>
2022-01-17 13:37:18 +0100

    check json status (validation)

--- matlab/ecat2nii.m
+++ matlab/ecat2nii.m
@@ -260,6 +260,10 @@ for j=1:length(FileListIn)
         info.ImageSize                        = [sh{1}.x_dimension sh{1}.y_dimension sh{1}.z_dimension mh.num_frames];
         info.PixelDimensions                  = [sh{1}.x_pixel_size sh{1}.y_pixel_size sh{1}.z_pixel_size 0].*10;
         jsonwrite([filenameout '.json'],info)
+        status = updatejsonpetfile([filenameout '.json']); % validate 
+        if status.state ~= 1
+            error('the json file appears invalid: %s',status.messages)
+        end
         
         info.Datatype                         = 'single';
         info.BitsPerPixel                     = 32;
