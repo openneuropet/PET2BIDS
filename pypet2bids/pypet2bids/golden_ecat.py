267c5a164a89fb87ea7b0ea6583b9eb6bbe159e6
anthony galassi <28850131+bendhouseart@users.noreply.github.com>
2022-01-20 14:22:47 -0500

    cleaned up branch

--- pypet2bids/pypet2bids/golden_ecat.py
+++ pypet2bids/pypet2bids/golden_ecat.py
@@ -3,58 +3,82 @@ import dotenv
 from write_ecat import *
 from read_ecat import read_ecat, ecat_header_maps
 import os
+from math import e
+from pathlib import Path
+'''
+This script exists solely to create an ecat with pixel values ranging from 0 to 32767 with a minimum number of frames.
+This evenly pixel spaced ecat that has a small number of frames will be used to evaluate the accuracy and precision
+of a number of tools that convert or otherwise manipulate ecat data by supplying a ecat image and a corresponding text
+file containing the pixel data of that ecat.
 
-# reference ecat
-# defaults???
-# make a class? maybe
-# check env for path to golden ecat file
+This ECAT file and script is thus referred to as the Golden ECAT b/c it represents the perfect standard by which to 
+judge all ECATs.
+
+The accompanying text file is formatted such that the pixel values are written one per line:
+
+0
+1
+2
+3
+.
+.
+.
+32767
+
+These values are then transformed into a set of NxNxNxF where N = the dimension of a matrix, and F = the total number of
+frames within the ecat. 
+
+Anthony Galassi - 2022
+----------------------------------------------
+Copyright Open NeuroPET team
+'''
+
+# Path to a reference/skeleton ecat as well as output paths for saving the created ecats to are stored in
+# environment variables or a .env file
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 # collect path to golden ecat file
-golden_ecat_path = os.environ['GOLDEN_ECAT']
+int_golden_ecat_path = os.environ['GOLDEN_ECAT_INTEGER']
 
-# collect skeleton header
+# collect skeleton header and subheaders.
 skeleton_main_header, skeleton_subheader, _ = read_ecat(os.environ['GOLDEN_ECAT_TEMPLATE_ECAT'],
                                                         collect_pixel_data=False)
-# collect skeleton directory table
-
-
 number_of_frames = 4
 one_dimension = 16
 # generate known 'pixel' data e.g. 4 frames with volume = 4x4x4
 number_of_array_elements = (one_dimension**3) * number_of_frames
 
-
-# -32768 and 32767
-image_min = -32768
+# first generate integer pixel data
+# 0 to 32767
+image_min = 0
 image_max = 32767
-spacing = int((image_max - image_min) / number_of_array_elements)
+spacing = round((image_max - image_min) / number_of_array_elements)
 
 integer_pixel_data = numpy.arange(image_min, image_max, dtype=numpy.int16, step=spacing)
 
+# now we write the integer pixel data to a file for testing
+integer_data_save_path = Path(int_golden_ecat_path)
+numpy.savetxt(integer_data_save_path.with_suffix('.txt'), integer_pixel_data, fmt='%d')
 
-while integer_pixel_data.size != number_of_array_elements:
-    integer_pixel_data = integer_pixel_data[:-1]
+# reshape pixel data into 3-d arrays
+pixels_to_collect = one_dimension ** 3
+frames = []
+temp_three_d_arrays = numpy.array_split(integer_pixel_data, number_of_frames)
+for i in range(number_of_frames):
+    frames.append(temp_three_d_arrays[i].reshape(one_dimension, one_dimension, one_dimension))
 
 # edit the header to suit the new file
 header_to_write = skeleton_main_header
-header_to_write['NUM_FRAMES'] = one_dimension
-header_to_write['ORIGINAL_FILE_NAME'] = 'GoldenECAT'
+header_to_write['NUM_FRAMES'] = 4
+header_to_write['ORIGINAL_FILE_NAME'] = 'GoldenECATInteger'
 header_to_write['STUDY_TYPE'] = 'Golden'
 header_to_write['PATIENT_ID'] = 'PerfectPatient'
 header_to_write['PATIENT_NAME'] = 'Majesty'
 header_to_write['FACILITY_NAME'] = 'Virtual'
+header_to_write['NUM_PLANES'] = one_dimension
 
-# Write header to file
-with open(golden_ecat_path, 'wb') as ecat_file:
-    write_header(ecat_file=ecat_file,
-                 schema=ecat_header_maps['ecat_headers']['73']['mainheader'],
-                 values=header_to_write)
-
-    # collect subset of skeleton frames
-    subheaders_to_write = skeleton_subheader[0:one_dimension]
-
-    for subheader in subheaders_to_write:
+subheaders_to_write = skeleton_subheader[0:number_of_frames]
+for subheader in subheaders_to_write:
         subheader['X_DIMENSION'] = one_dimension  # pixel data is 3-d this is 1/3 root of the 3d array.
         subheader['Y_DIMENSION'] = one_dimension
         subheader['Z_DIMENSION'] = one_dimension
@@ -62,40 +86,22 @@ with open(golden_ecat_path, 'wb') as ecat_file:
         subheader['IMAGE_MAX'] = integer_pixel_data.max()
         subheader['ANNOTATION'] = 'This patient is very small.'
         subheader['DATA_TYPE'] = 6
-
-    # generate a directory table for the golden ecat
-    directory = create_directory_table(
-        num_frames=number_of_frames,
-        pixel_dimensions={'x': one_dimension, 'y': one_dimension, 'z': one_dimension},
-        pixel_byte_size=2)
-
-    # write the directory to file
-    after_table_position = write_directory_table(ecat_file, directory)
-    seek_position = ecat_file.tell()
-    assert seek_position == after_table_position
-
-    # move forward 512 bytes as directory tables may be 1024 bytes, this one however is only 512
-    ecat_file.seek(512, 1)
-
-    # reshape pixel data into 3-d arrays
-    pixels_to_collect = one_dimension ** 3
-    frames = []
-    temp_three_d_arrays = numpy.array_split(integer_pixel_data, number_of_frames)
-    for i in range(number_of_frames):
-        frames.append(temp_three_d_arrays[i].reshape(one_dimension, one_dimension, one_dimension))
-
-    # write frame data (subheaders and pixel data)
-    # for the number of entries in the directory
-    for index in range(directory[0][3, 0]):
-        subheader_and_frame_byte_position = 512 * directory[0][1, index + 1]
-        assert ecat_file.tell() == subheader_and_frame_byte_position
-        pixel_seek = write_header(ecat_file=ecat_file,
-                                  schema=ecat_header_maps['ecat_headers']['73']['7'],
-                                  values=subheaders_to_write[index])
-
-        assert ecat_file.tell() == subheader_and_frame_byte_position + 512
-        write_pixel_data(ecat_file=ecat_file,
-                         pixel_data=frames[index])
+        subheader['SCALE_FACTOR'] = 1
 
 
-golden_ecat_main_header, golden_ecat_subheaders, golden_ecat_pixel_data = read_ecat(golden_ecat_path)
+
+write_ecat(ecat_file=int_golden_ecat_path,
+           mainheader_schema=ecat_header_maps['ecat_headers']['73']['mainheader'],
+           mainheader_values=header_to_write,
+           subheaders_values=subheaders_to_write,
+           subheader_schema=ecat_header_maps['ecat_headers']['73']['7'],
+           number_of_frames=number_of_frames,
+           pixel_x_dimension=one_dimension,
+           pixel_y_dimension=one_dimension,
+           pixel_z_dimension=one_dimension,
+           pixel_byte_size=2,
+           pixel_data=frames
+           )
+
+# now read it just to make sure what was created is a real file and not error riddled.
+int_golden_ecat_main_header, int_golden_ecat_subheaders, int_golden_ecat_pixel_data = read_ecat(int_golden_ecat_path)
