639daba4cad6869d76979f872be2c10ca82143cf
CPernet <wamcyril@gmail.com>
2022-01-31 21:48:00 +0100

    use BIDS keys + load BIDS keys to run them all

--- matlab/get_pet_metadata.m
+++ matlab/get_pet_metadata.m
@@ -1,17 +1,17 @@
 function metadata = get_pet_metadata(varargin)
 
-% Routine that outputs PET scanner metadata following 
+% Routine that outputs PET scanner metadata following
 % <https://bids.neuroimaging.io/ BIDS>
 %
 % Such PET scanner metadata are passed along imaging data to the
-% converters, ecat2nii.m or dcm2niix4pet.m allowing to have fully 
+% converters, ecat2nii.m or dcm2niix4pet.m allowing to have fully
 % compliant json files alongside the .nii files
 %
-% Defaults parameters (aquisition and reconstruction parameters) should be 
+% Defaults parameters (aquisition and reconstruction parameters) should be
 % stored in a *_parameters.txt seating on disk next to this function
 % or passed as argument in. Replace * by the name of your scanner - for now we
-% support 'SiemensBiograph', 'SiemensHRRT', 'GEAdvance' and 'PhillipsVereos'. 
-% (see templates, as some info can be recovered from ecat or dcm - ie not 
+% support 'SiemensBiograph', 'SiemensHRRT', 'GEAdvance' and 'PhillipsVereos'.
+% (see templates, as some info can be recovered from ecat or dcm - ie not
 % all info is necessarily needed)
 %
 % Here is an example of such defaults, used at NRU for our SiemensBiograph_parameters.txt
@@ -27,21 +27,21 @@ function metadata = get_pet_metadata(varargin)
 % ReconFilterType                = 'XYZGAUSSIAN';
 % ReconFilterSize                = 2;
 % AttenuationCorrection          = 'CT-based attenuation correction';
-%                   
+%
 % FORMAT:  metadata = get_pet_metadata(key,value)
 %
 % Example: metadata = get_pet_metadata('Scanner','SiemensBiograph','TimeZero','ScanStart',...
-%                        'tracer','AZ10416936','Radionuclide','C11', 'ModeOfAdministration','bolus',...
-%                        'Radioactivity', 605.3220,'InjectedMass', 1.5934,'MolarActivity', 107.66)
+%                        'TracerName','AZ10416936','TracerRadionuclide','C11', 'ModeOfAdministration','bolus',...
+%                        'InjectedRadioactivity', 605.3220,'InjectedMass', 1.5934,'MolarActivity', 107.66)
 %
 % INPUTS a series of key/value pairs are expected
 %        MANDATORY
 %                  Scanner: name of scanner, map to a *parameters.txt file  e.g. 'Scanner', 'SiemensBiograph'
 %                  TimeZero: when was the tracer injected                   e.g. 'TimeZero','11:05:01'
 %                  ModeOfAdministration                                     e.g. 'ModeOfAdministration', 'bolus'
-%                  tracer: which tracer was used                            e.g. 'tracer','DASB'
-%                  Radionuclide: which nuclide was used                     e.g. 'Radionuclide','C11'
-%                  Injected Radioactivity Dose: value in MBq                e.g. 'Radioactivity', 605.3220
+%                  TracerName: which tracer was used                        e.g. 'TracerName','DASB'
+%                  TracerRadionuclide: which nuclide was used               e.g. 'TracerRadionuclide','C11'
+%                  InjectedRadioactivity: value in MBq                      e.g. 'InjectedRadioactivity', 605.3220
 %                  InjectedMass: Value in ug                                e.g. 'InjectedMass', 1.5934
 %                  + at least 2 of those key/value arguments:
 %                  MolarActivity: value in GBq/umol                         e.g. 'MolarActivity', 107.66
@@ -52,21 +52,11 @@ function metadata = get_pet_metadata(varargin)
 %        the scanning time should be used as TimeZero, this will be filled
 %        out automatically by ecat2nii
 %
-%        OPTIONAL INPUTS
-%                  AcquisitionMode             
-%                  ImageDecayCorrected           
-%                  ImageDecayCorrectionTime      
-%                  ReconMethodName                
-%                  ReconMethodParameterLabels     
-%                  ReconMethodParameterUnits      
-%                  ReconMethodParameterValues     
-%                  ReconFilterType                
-%                  ReconFilterSize               
-%                  AttenuationCorrection          
-% 
+%        OPTIONAL INPUTS ARE ALL OTHER BIDS FIELDS
+%
 % OUTPUT metadata is a structure with BIDS fields filled
 %        (such structure is ready to be writen as json file using e.g.
-%        the bids matlab jsonwrite function, typically associated with the *_pet.nii file) 
+%        the bids matlab jsonwrite function, typically associated with the *_pet.nii file)
 %
 % Neurobiology Research Unit, Rigshospitalet
 % Martin Nørgaard & Cyril Pernet - 2021
@@ -80,102 +70,119 @@ function metadata = get_pet_metadata(varargin)
 if nargin == 0
     help get_pet_metadata
     return
-else    
-    for n=1:2:nargin
+else
+    for n=1:2:nargin % check the mandatory variables
         if strcmpi(varargin{n},'Scanner')
             Scanner = varargin{n+1};
         elseif any(strcmpi(varargin{n},{'TimeZero','Time Zero'}))
             TimeZero = varargin{n+1};
-        elseif strcmpi(varargin{n},'tracer')
-            tracer = varargin{n+1};
-        elseif strcmpi(varargin{n},'Radionuclide')
-            Radionuclide = varargin{n+1};
-        elseif contains(varargin{n},'Radioactivity','IgnoreCase',true)
+        elseif strcmpi(varargin{n},'TracerName')
+            TracerName = varargin{n+1};
+        elseif strcmpi(varargin{n},'TracerRadionuclide')
+            TracerRadionuclide = varargin{n+1};
+        elseif contains(varargin{n},'Administration','IgnoreCase',true)
+            ModeOfAdministration = varargin{n+1};
+        elseif contains(varargin{n},{'InjectedRadioactivity','Injected Radioactivity'},'IgnoreCase',true)
             InjectedRadioactivity = varargin{n+1};
+        elseif contains(varargin{n},{'SpecificRadioactivity','Specific Radioactivity'},'IgnoreCase',true)
+            SpecificRadioactivity = varargin{n+1};
         elseif contains(varargin{n},'Mass','IgnoreCase',true)
             InjectedMass = varargin{n+1};
         elseif any(strcmpi(varargin{n},{'MolarActivity','Molar Activity'}))
             MolarActivity = varargin{n+1};
         elseif contains(varargin{n},'Weight','IgnoreCase',true)
             MolecularWeight = varargin{n+1};
-        elseif contains(varargin{n},'Administration','IgnoreCase',true)
-            ModeOfAdministration = varargin{n+1};
-        elseif contains(varargin{n},'InstitutionName','IgnoreCase',true)
-            InstitutionName = varargin{n+1};
-        elseif any(strcmpi(varargin{n},{'AcquisitionMode','Acquisition Mode'}))
-            AcquisitionMode = varargin{n+1};
-        elseif contains(varargin{n},'DecayCorrected','IgnoreCase',true)
-            ImageDecayCorrected = varargin{n+1};
-        elseif contains(varargin{n},'DecayCorrectionTime','IgnoreCase',true)
-            ImageDecayCorrectionTime = varargin{n+1};
-        elseif contains(varargin{n},'MethodName','IgnoreCase',true)
-            ReconMethodName = varargin{n+1};
-        elseif contains(varargin{n},'FilterType','IgnoreCase',true)
-            ReconFilterType = varargin{n+1};
-        elseif contains(varargin{n},'FilterSize','IgnoreCase',true)
-            ReconFilterSize = varargin{n+1};
-        elseif contains(varargin{n},'AttenuationCorrection','IgnoreCase',true)
-            AttenuationCorrection = varargin{n+1};
         end
     end
     
     % check input values and consistency given expected units in
-    % -----------------------------------------------------------   
+    % -----------------------------------------------------------
+    mandatory = {'Scanner','TimeZero','TracerName','ModeOfAdministration','TracerRadionuclide'};
     radioinputs = {'InjectedRadioactivity', 'InjectedMass', ...
         'SpecificRadioactivity', 'MolarActivity', 'MolecularWeight'};
     input_check = cellfun(@exist,radioinputs);
+    if isempty(input_check)
+        error('radioactivity related input are necessary - see help')
+    end
+    
+    index = 1; % make key-value pairs
     arguments   = cell(1,sum(input_check)*2);
-    index = 1;
-    for r=1:length(radioinputs)
-        if input_check(r)
-            arguments{index}   = radioinputs{r};
-            arguments{index+1} = eval(radioinputs{r});
-            index = index + 2;
-        end
+    for r=find(input_check)
+        arguments{index}   = radioinputs{r};
+        arguments{index+1} = eval(radioinputs{r});
+        index = index + 2;
     end
     dataout  = check_metaradioinputs(arguments);
-    setval   = fieldnames(dataout);
-    for r=1:length(setval)
-        if isnumeric(dataout.(setval{r}))
-            eval([setval{r} '=' num2str(dataout.(setval{r}))])
-        else
-            eval([setval{r} '=''' dataout.(setval{r}) ''''])
+    if isempty(dataout)
+        error('there are not enough radioactivity related inputs to make sense of the data - see help')
+    end
+    
+    % set old and new variables in memory
+    setval   = fieldnames(dataout) ; 
+    for f = 1:2:length(arguments) % dataout might not include all arguments in
+        if ~any(strcmpi(arguments{f},setval))
+            setval(length(setval)+1) = arguments(f);
         end
     end
     
-    % check mandatory/optional fileds of this function (not the BIDS fields)
-    mandatory = {'Scanner','TimeZero','tracer','ModeOfAdministration','Radionuclide','InjectedRadioactivity','InjectedMass'};
+    for r=1:length(setval)
+        mandatory{length(mandatory)+1} = setval{r}; % add to mandatory list
+        if isfield(dataout,setval{r})
+            if isnumeric(dataout.(setval{r}))
+                eval([setval{r} '=' num2str(dataout.(setval{r}))])
+            else
+                eval([setval{r} '=''' dataout.(setval{r}) ''''])
+            end
+        end
+    end
+    
+    % check mandatory/optional fields of this function
+    % (same fields but different status as BIDS)
     if ~all(cellfun(@exist, mandatory))
         error('One or more mandatory name/value pairs are missing \n%s',mandatory{find(cellfun(@exist, mandatory)==0)})
     end
     
-    optional = {'InstitutionName',...
-                'AcquisitionMode',...
-                'ImageDecayCorrected',...
-                'ImageDecayCorrectionTime',...
-                'ReconMethodName',...
-                'ReconMethodParameterLabels',...
-                'ReconMethodParameterUnits',...
-                'ReconMethodParameterValues',...
-                'ReconFilterType',...
-                'ReconFilterSize',...
-                'AttenuationCorrection'};
-            parameter_file = fullfile(fileparts(which('get_pet_metadata.m')),[Scanner 'parameters.txt']);
+    current    = which('get_pet_metadata.m');
+    root       = current(1:strfind(current,'converter')+length('converter'));
+    jsontoload = fullfile(root,['metadata' filesep 'PET_metadata.json']);
+    if exist(jsontoload,'file')
+        petmetadata = jsondecode(fileread(jsontoload));
+        optional    = [petmetadata.mandatory' petmetadata.recommended' petmetadata.optional']';
+        for f=2:length(mandatory) % start at 2 (1 is Scanner)
+            remove = find(cellfun(@(x) strcmpi(x, mandatory{f}),optional));
+            if ~isempty(remove)
+               optional(remove) = [];
+            end
+        end
+    else
+        error('looking for %s, but the file is missing',jsontoload)
+    end
+    
+    % evaluate key-value pairs for optional arguments in
+    for n=1:2:nargin 
+        if any(strcmpi(varargin{n},optional))
+            if isnumeric(varargin{n+1})
+                eval([varargin{n} '=' num2str(varargin{n+1})])
+            else
+                eval([varargin{n} '=''' varargin{n+1} ''''])
+            end
+        end
+    end
+     
+    % evaluate key-value pairs for optional arguments from txt file
+    parameter_file = fullfile(fileparts(which('get_pet_metadata.m')),[Scanner 'parameters.txt']);
     if ~any(cellfun(@exist, optional))
         if exist(parameter_file,'file')
             setmetadata = importdata(parameter_file);
-            for opt = 1:length(optional)
-                if ~exist(optional{opt},'var')
-                    option_index = find(contains(setmetadata,optional{opt}));
-                    if ~isempty(option_index)
-                        try
-                            eval(setmetadata{option_index}); % shoul evaluate the = sign, creating name/value pairs                end
-                            if isempty(eval(optional{opt}))
-                                error('''%s'' from %sparameters.txt is empty\n',optional{opt},Scanner)
-                            end
-                        catch evalerr
-                            error('''%s'' from %sparameters.txt is empty\n',optional{opt},Scanner)% --> error for optional inputs
+            for opt = 1:length(setmetadata)
+                if contains(setmetadata{opt},'=')
+                    try
+                        eval(setmetadata{opt}); % shoul evaluate the = sign, creating name/value pairs                end
+                        if isempty(setmetadata{opt})
+                            error('''%s'' from %sparameters.txt is empty\n',optional{opt},Scanner)
                         end
+                    catch evalerr
+                        error('''%s'' from %sparameters.txt is empty\n',setmetadata{opt},Scanner)% --> error for optional inputs
                     end
                 end
             end
@@ -191,6 +198,8 @@ else
                 'ReconFilterType            = '''';',...
                 'ReconFilterSize            = ;',...
                 'AttenuationCorrection      = '''';'}',...
+                'FrameDuration              = [ , ];',...
+                'FrameTimesStart            = [ , ];',...
                 'VariableNames',{'# Defaults'});
             writetable(T,parameter_file);
             error([Scanner 'parameters.txt to load default parameters is missing - a template file has been created, please fill missing information, or pass them as arguments in'])
@@ -225,9 +234,9 @@ elseif contains(Scanner,'GE','IgnoreCase',true)
 elseif contains(Scanner,'Philips','IgnoreCase',true)
     metadata.Manufacturer = 'Philips';
     if contains(Scanner,'Vereos','IgnoreCase',true)
-        metadata.ManufacturersModelName = 'Vereos PET/CT'; 
+        metadata.ManufacturersModelName = 'Vereos PET/CT';
     elseif contains(Scanner,'Ingenuity','IgnoreCase',true)
-        metadata.ManufacturersModelName = 'Ingenuity TF PET/CT'; 
+        metadata.ManufacturersModelName = 'Ingenuity TF PET/CT';
     else
         loc = find([strfind(Scanner,{'Philips'}) strfind(Scanner,{'philips'})]);
         Scanner(loc:loc+length('Philips')-1) = [];
@@ -245,18 +254,26 @@ if isempty(TimeZero)
 elseif strcmpi(varargin{n},{'ScanStart','Scan Start'})
     metadata.TimeZero                   = 'ScanStart';
 else
-    metadata.TimeZero                   = TimeZero;   
+    metadata.TimeZero                   = TimeZero;
 end
 metadata.TimeZero                       = TimeZero;
 metadata.ModeOfAdministration           = ModeOfAdministration;
-metadata.TracerName                     = tracer;
-metadata.TracerRadionuclide             = Radionuclide;
-metadata.InjectedRadioactivity          = InjectedRadioactivity;
-metadata.InjectedRadioactivityUnits     = 'MBq';
-metadata.InjectedMass                   = InjectedMass;
-metadata.InjectedMassUnits              = 'ug';
-metadata.SpecificRadioactivity          = SpecificRadioactivity;
-metadata.SpecificRadioactivityUnits     = SpecificRadioactivityUnits;
+metadata.TracerName                     = TracerName;
+metadata.TracerRadionuclide             = TracerRadionuclide;
+
+if exist('InjectedRadioactivity', 'var')
+    metadata.InjectedRadioactivity          = InjectedRadioactivity;
+    metadata.InjectedRadioactivityUnits     = 'MBq';
+end
+
+if exist('InjectedMass', 'var')
+    metadata.InjectedMass                   = InjectedMass;
+    metadata.InjectedMassUnits              = 'ug';
+end
+if exist('SpecificRadioactivity', 'var')
+    metadata.SpecificRadioactivity          = SpecificRadioactivity;
+    metadata.SpecificRadioactivityUnits     = SpecificRadioactivityUnits;
+end
 
 if exist('MolecularWeight', 'var')
     metadata.TracerMolecularWeight      = MolecularWeight;
@@ -268,46 +285,9 @@ if exist('MolarActivity', 'var')
     metadata.MolarActivityUnits         = 'GBq/umol';
 end
 
-if exist('InstitutionName','var')
-    metadata.InstitutionName            = InstitutionName;
+% run through option setting dynamic fields
+for f=1:length(optional)
+    if exist(optional{f},'var')
+        metadata.(optional{f}) = eval(optional{f});
+    end
 end
-
-if exist('AcquisitionMode','var')
-    metadata.AcquisitionMode            = AcquisitionMode;
-end
-
-if exist('ImageDecayCorrected','var')
-    metadata.ImageDecayCorrected        = ImageDecayCorrected;
-end
-if exist('ImageDecayCorrectionTime','var')
-    metadata.ImageDecayCorrectionTime   = ImageDecayCorrectionTime;
-end
-
-if exist('ReconMethodName','var')
-    metadata.ReconMethodName            = ReconMethodName;
-end
-
- if exist('ReconMethodParameterLabels','var')
-    metadata.ReconMethodParameterLabels = ReconMethodParameterLabels;
- end
- 
- if exist('ReconMethodParameterUnits','var')
-    metadata.ReconMethodParameterUnits  = ReconMethodParameterUnits;
- end
- 
- if exist('ReconMethodParameterValues','var')
-    metadata.ReconMethodParameterValues = ReconMethodParameterValues;
-end
-
-if exist('ReconFilterType','var')
-    metadata.ReconFilterType            = ReconFilterType;
-end
-
-if exist('ReconFilterSize','var')
-    metadata.ReconFilterSize            = ReconFilterSize;
-end
-
-if exist('AttenuationCorrection','var')
-    metadata.AttenuationCorrection      = AttenuationCorrection;
-end
-
