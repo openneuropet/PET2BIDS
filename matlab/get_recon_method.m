d6724a5f6a7cf2ed7564f635e6773d73dfa651e6
CPernet <wamcyril@gmail.com>
2022-05-10 06:12:06 +0200

    find the method along with i and s if any

---
+++ matlab/get_recon_method.m
@@ -0,0 +1,112 @@
+function [method,iteration,subset] = get_recon_method(headervalue)
+
+% from the dicom value of the PET reconstruction method, it returns the
+% actual name of the method and the number of iterations and subsets if any
+%
+% :format: - [method,iteration,subset] = get_recon_method(headervalue)
+%
+% :param headervalue: the name in the ecat or dicom field of the reconstrusction method
+%                     typically includes i and s for iteration and subset (unless bach projection was used)
+%
+% :returns method: the full name of the reconstrucrtion method or empty if no match found
+% :returns iteration: the number of iterations or 'none'
+% :returns subset: the number of subsets or 'none'
+
+
+%% defaults
+method    = [];
+iteration = 'none';
+subset    = 'none';
+
+%% load metadata from the library
+current    = which('ecat2nii.m');
+root       = current(1:strfind(current,'converter')+length('converter'));
+jsontoload = fullfile(root,['metadata' filesep 'PET_reconstruction_methods.json']);
+if exist(jsontoload,'file')
+    reconstruction_metadata = jsondecode(fileread(jsontoload));
+    reconstruction_metadata = reconstruction_metadata.reconstruction_names;
+    values = arrayfun(@(x) x.value, reconstruction_metadata, 'UniformOutput', false);
+    names  = arrayfun(@(x) x.name, reconstruction_metadata, 'UniformOutput', false);
+else
+    error('looking for %s, but the file is missing',jsontoload)
+end
+
+%% do some matching
+
+% get iterations and subsets
+dim                = []; % if 3D becomes '3D' to append to names
+dim_position       = []; % position to clean-up the name
+iteration_position = [];
+subset_position    = [];
+numbers            = regexp(headervalue,'\d*','Match');
+for n=1:length(numbers)
+    loc        = strfind(headervalue,numbers{n});
+    loc_length = loc+length(numbers{n})-1;
+    
+    if strcmpi(headervalue(loc:loc+1),'3D')
+        dim          = '3D'; 
+        dim_position = loc:loc+1;
+    end
+    
+    if loc_length<length(headervalue)       
+        if strcmpi([headervalue(loc-1) headervalue(loc:loc_length)],['i',numbers{n}])
+            iteration          = numbers{n};
+            iteration_position = loc-1:loc_length;
+        elseif strcmpi([headervalue(loc:loc_length) headervalue(loc_length+1)],[numbers{n} 'i'])
+            iteration          = numbers{n};
+            iteration_position = loc:loc_length+1;
+        elseif strcmpi([headervalue(loc-1) headervalue(loc:loc_length)],['s',numbers{n}]) 
+            subset            = numbers{n};
+            subset_position   = loc-1:loc_length;
+        elseif strcmpi([headervalue(loc:loc_length) headervalue(loc_length+1)],[numbers{n} 's'])
+            subset            = numbers{n};
+            subset_position   = loc:loc_length+1;
+        end
+    else
+        if strcmpi([headervalue(loc-1) headervalue(loc:loc_length)],['i',numbers{n}]) 
+            iteration = numbers{n};
+            iteration_position = loc-1:loc_length;
+        elseif strcmpi([headervalue(loc:loc_length) headervalue(loc_length)],[numbers{n} 'i'])
+            iteration = numbers{n};
+            iteration_position = loc:loc_length;
+        elseif strcmpi([headervalue(loc-1) headervalue(loc:loc_length)],['s',numbers{n}])
+            subset          = numbers{n};
+            subset_position = loc-1:loc_length;
+        elseif strcmpi([headervalue(loc:loc_length) headervalue(loc_length)],[numbers{n} 's'])
+            subset          = numbers{n};
+            subset_position = loc:loc_length;
+        end
+    end
+end
+
+if strcmp(iteration,'none') && ~strcmpi(subset,'none') || ...
+        ~strcmp(iteration,'none') && strcmpi(subset,'none')
+    warning('subset or iteration found but not the other, something is wrong in the metadata reconstruction information')
+end
+
+% clean the name of the method remove 3D, i, s and numbers
+clean_name = headervalue; % fall back on this if name detection fails
+if ~isempty(iteration_position) || ~isempty(subset_position)
+    remove = [dim_position iteration_position subset_position];
+    headervalue(remove) = [];
+    remove = [iteration_position subset_position];
+    clean_name(remove) = [];
+end
+
+% try to match the library with looking at bits of names and reassemble
+for v = 1:length(values)
+    if ~isempty(strfind(headervalue,values{v}))
+        if any(strcmpi(values{v},{'OSEM','LARMA'}))
+            method = [method dim ' ' names{v} ' '];
+        else
+            method = [method names{v} ' '];
+        end
+    end
+end
+method = deblank(method);
+
+if isempty(method )
+    warning('the reconstruction method is not in our library, check metadata json (and get in touch to include it)')
+    warning('using method %s',clean_name)
+    method = clean_name;
+end
