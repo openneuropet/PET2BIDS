d2389e067b722a2b22c6aff5d242377aeb3caede
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2022-05-19 17:18:45 -0400

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ copyright = '2022, OpenNeuroPET'
 author = 'OpenNeuroPET'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.10g'
+release = '0.0.11'
 
 # -- General configuration ---------------------------------------------------
 
