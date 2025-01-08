9a99d912a78c7c2592f3e5193950ac7f51f3af13
fishpm <patrick.fisher@gmail.com>
2025-01-08 10:51:39 +0100

    rescaling factor no longer written to .txt file
    Commented two lines so that ecat2nii no longer tries to write rescaling factor to .txt file

--- matlab/ecat2nii.m
+++ matlab/ecat2nii.m
@@ -212,11 +212,11 @@ for j=1:length(FileListIn)
             end
 
             % save scaling factor to file located at ecat_save_steps_dir/8.5_sca_matlab.txt
-            fid = fopen([ecat_save_steps_dir filesep '8.5_sca_matlab.txt'],'w');
+            % fid = fopen([ecat_save_steps_dir filesep '8.5_sca_matlab.txt'],'w');
             fprintf(fid,'Scaling factor: %10e\n',Sca);
             x = mh.ecat_calibration_factor * Sca;
             fprintf(fid,'Scaling factor * ECAT Cal Factor: %10.10f\n',x);
-            fclose(fid);
+            % fclose(fid);
         end
 
         % save debugging step 8 - rescale to 16 bits
