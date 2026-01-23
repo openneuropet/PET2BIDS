ca1fd615009733bdfed41b97886510bd34038d79
Keith George Ciantar <52758149+KeithGeorgeCiantar@users.noreply.github.com>
2026-01-23 15:09:51 +0100

    Fix scaling calculations for image data to match the MATLAB version

--- pypet2bids/pypet2bids/ecat2nii.py
+++ pypet2bids/pypet2bids/ecat2nii.py
@@ -228,12 +228,12 @@ def ecat2nii(
     rg = img_temp.max() - img_temp.min()
     if rg != 32767:
         max_img = img_temp.max()
-        img_temp = img_temp / max_img * 32767
+        img_temp = (img_temp / max_img) * 32767
         sca = max_img / 32767
         min_img = img_temp.min()
         if min_img < -32768:
-            img_temp = img_temp / (min_img * -32768)
-            sca = sca * (min_img * -32768)
+            img_temp = (img_temp / min_img) * (-32768)
+            sca = (sca * min_img) / (-32768)
     if ecat_save_steps == "1":
         with open(os.path.join(steps_dir, "8.5_sca.txt"), "w") as sca_file:
             sca_file.write(f"Scaling factor: {sca}\n")
